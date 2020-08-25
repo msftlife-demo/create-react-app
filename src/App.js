8cc78b1864ad5e2d914b75751d1b08a7fd27c28d
Eunji Kim <eunk@microsoft.com>
2020-08-25 23:30:10 +0900

    Change comment AB#5

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-        Learn React
+        Hello Girls in Tech!
         </a>
       </header>
     </div>
