95e5ded77aab8ff64e3211f964e2d59458153074
Eunji Kim <eunk.angie@outlook.com>
2020-08-25 09:53:36 +0900

    change test name AB#5

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Hello Girls In Tech link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Hello Girls In Tech!/i);
   expect(linkElement).toBeInTheDocument();
