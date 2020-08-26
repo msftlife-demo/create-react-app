176be3e7e0124db279c7a55bf2b169a997cad766
Eunji Kim <eunk@microsoft.com>
2020-08-26 20:06:29 +0900

    test code update

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders Learn React link', () => {
+test('renders Hello Girls In Tech link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Hello Girls In Tech/i);
   expect(linkElement).toBeInTheDocument();
 });
